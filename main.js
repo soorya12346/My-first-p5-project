89d9ade08c48cdace3164a1b399b58cc1ee9367e
soorya12346 <75780414+soorya12346@users.noreply.github.com>
2020-12-10 14:23:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+}
+function setup(){
+canvas=creatCanvas(640,480);
+canvas.position(110,250);
+video=createCapture(VIDEO);
+video.hide();
+tint_color="";
+}
+
+function draw(){
+image(video,0,0,640,480);
+tint(tint_color);
+
+}
+function draw
